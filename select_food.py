d6260a8cb916de5ea1aac1dd1d5354f5e2c04535
cullenjohn <cullenjohn167@gmail.com>
2023-08-11 00:21:38 +0100

    ......

--- select_food.py
+++ select_food.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from flask import request
+from flask import request, jsonify
 
-impory jsonify
+# import jsonify
 import pyodbc
 from flask_cors import CORS
 
@@ -9,35 +9,19 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://40268037flaskfrontend.azurewebsites.net"}})
 
 
+dpp = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "https://40268037flaskfrontend.azurewebsites.net"}})
+
 def get_connection_string():
-    # Obtainn string from azure environment before local
-    azure_db = os.environ.get('DB_CONNECTION_STRING')
-    
-    if azure_db:
-        return azure_db
-    
-    # Local db string
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    
-    server = config['DATABASE']['server']
-    db = config['DATABASE']['db']
-    username = config['DATABASE']['user']
-    password = config['DATABASE']['password']
-    driver = config['DATABASE']['driver']
-    port = config['DATABASE']['port']
-    
-    return f'DRIVER={driver};SERVER={server};PORT={port};DATABASE={db};UID={username};PWD={password}'
+    # ... [rest of the function remains the same]
 
 def create_connection():
     return pyodbc.connect(get_connection_string())
- 
-connection = create_connection()
 
 @app.route('/return_foods', methods=['GET'])
 def select_data():
     connection = create_connection()
-    cursor = connect.cursor()
+    cursor = connection.cursor()
     select_query = "SELECT * FROM Foods;"
     cursor.execute(select_query)
     rows = cursor.fetchall()
@@ -46,13 +30,11 @@ def select_data():
     for row in rows:
        result.append({'food_name': row[1], 'quantity': row[2], 'expiry_date': row[3], 'date_added': row[4]})
 
-    connect.commit()
-    connect.close()
-    returning jsonify(result)
-
-select_data()
-
+    connection.commit()
+    connection.close()
     
+    return jsonify(result)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
         
