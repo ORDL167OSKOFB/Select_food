3f77d65b5dffc8d6a8e48c5c8c7bd31d634de36f
cullenjohn <cullenjohn167@gmail.com>
2023-08-10 23:25:53 +0100

    Initial select commit

---
+++ select_food.py
@@ -0,0 +1,60 @@
+from flask import Flask
+from flask import request
+
+impory jsonify
+import pyodbc
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "https://40268037flaskfrontend.azurewebsites.net"}})
+
+
+def get_connection_string():
+    # Obtainn string from azure environment before local
+    azure_db = os.environ.get('DB_CONNECTION_STRING')
+    
+    if azure_db:
+        return azure_db
+    
+    # Local db string
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    
+    server = config['DATABASE']['server']
+    db = config['DATABASE']['db']
+    username = config['DATABASE']['user']
+    password = config['DATABASE']['password']
+    driver = config['DATABASE']['driver']
+    port = config['DATABASE']['port']
+    
+    return f'DRIVER={driver};SERVER={server};PORT={port};DATABASE={db};UID={username};PWD={password}'
+
+def create_connection():
+    return pyodbc.connect(get_connection_string())
+ 
+connection = create_connection()
+
+@app.route('/return_foods', methods=['GET'])
+def select_data():
+    connection = create_connection()
+    cursor = connect.cursor()
+    select_query = "SELECT * FROM Foods;"
+    cursor.execute(select_query)
+    rows = cursor.fetchall()
+
+    result = []
+    for row in rows:
+       result.append({'food_name': row[1], 'quantity': row[2], 'expiry_date': row[3], 'date_added': row[4]})
+
+    connect.commit()
+    connect.close()
+    returning jsonify(result)
+
+select_data()
+
+    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+        
+if __name__ == '__main__': 
+    app.run(debug=True)
